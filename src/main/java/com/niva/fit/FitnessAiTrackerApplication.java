8c87a1274e9c7685604e9fa95b73a6cebfa0c9ed
Niva Maharjan <avinmhrz@gmail.com>
2026-02-08 13:04:05 -0500

    Spring Boot + Mongo DB set up

---
+++ src/main/java/com/niva/fit/FitnessAiTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.niva.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAiTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAiTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niva/fit/FitnessAiTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niva.fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAiTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
